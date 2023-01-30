dce7db3c1da1cc407235ea0f0d49c3baebb7a3df
Jhuneah <99521733+Jhuneah@users.noreply.github.com>
2023-01-30 20:35:44 +0000

    Delete download directory

--- download/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from download.models import FORMAT
-
-# Register your models here.
-admin.site.register(FORMAT)
--- download/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from download import views
-
-urlpatterns = [
-    path("",views.index,name='index'),
-    path("index",views.index,name='index'),
-]
